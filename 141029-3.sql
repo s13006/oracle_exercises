cc93e3dc44f2fbd5857209f0b06e64e388e78ffc
s13006 <s13006@std.it-college.ac.jp>
2014-10-29 14:35:45 +0900

    修正２

--- 141029-3.sql
+++ 141029-3.sql
@@ -1,5 +1,5 @@
 SELECT empno, ename, sal, deptno
 FROM employees
-WHERE 1003 <= ANY (SELECT empno FROM employees WHERE empno >= 1003)
+WHERE sal >= (SELECT sal FROM employees WHERE empno = 1003)
 AND deptno = (SELECT deptno FROM departments WHERE dname = '営業')
 /
