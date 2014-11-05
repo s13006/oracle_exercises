cb85af0cbca6835ab5836018730650b5d67c928d
s13006 <s13006@std.it-college.ac.jp>
2014-11-05 13:31:39 +0900

    修正

--- 141104-3.sql
+++ 141104-3.sql
@@ -1,11 +1,11 @@
 INSERT INTO employees(empno, ename, hiredate)
-VALUES (&empno, '&ename', TO_DATE('&年-&月-&日','YYYY-MM-DD'));
+VALUES (&empno, '&&ename', TO_DATE('&年-&月-&日','YYYY-MM-DD'));
 commit;
 
 SELECT empno, ename, hiredate FROM employees;
 
-DELETE FROM employees;
-WHERE ename = &ename;
+DELETE FROM employees
+WHERE ename = '&ename';
 commit;
 
 SELECT empno, ename, hiredate FROM employees;
